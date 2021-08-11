2ba79134ffad3c46c47c11c4fadd32b10459d7f4
Rikitei628 <78311581+Rikitei628@users.noreply.github.com>
2021-08-11 22:54:32 +0900

    delete "public"

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ const port = process.env.PORT || 3000
 app.get('/test', (req, res) => {
     res.send('Hello World!')
   })
-app.get('/', (req, res, next) =>{
-    res.redirect('/public')
-})
-app.use('/public' , express.static(path.resolve(__dirname, '../docs')))
+// app.get('/', (req, res, next) =>{
+//     res.redirect('/public')
+// })
+app.use('/' , express.static(path.resolve(__dirname, '../docs')))
 
 app.listen(port, ()=>{
     console.log(`Server started! Access to http://localhost:${port}`)
